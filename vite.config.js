5b01f61118521941d8f825f930424e2c5a9e7679
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-04 18:45:40 +0900

    chore: Update env base to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/react-todolist-newbie/',
+  base: import.meta.env.MODE === 'production' ? '/react-todolist-newbie/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/react-todolist-newbie/' : '/',
   plugins: [react()],
 })
