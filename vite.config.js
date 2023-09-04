bb5a1f65504852cc4d8eec94bb66cf66340ac83b
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-04 18:34:47 +0900

    chore: Update to remove base
    - try github workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-todolist-newbie/',
+  // base: '/react-todolist-newbie/',
   plugins: [react()],
 })
